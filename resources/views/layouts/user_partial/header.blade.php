d9f68445b71487dae7f6806aa4256737284fa88c
andypratama3 <andypratama1211@gmail.com>
2024-05-10 18:15:16 +0800

    feat: button show in buku when before

--- resources/views/layouts/user_partial/header.blade.php
+++ resources/views/layouts/user_partial/header.blade.php
@@ -1,19 +1,16 @@
 <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
 
-        <a href="{{ route('index') }}" class="logo me-auto"><img src="{{ asset('assets_user/img/logo1.jpeg') }}" alt="" class="img-fluid"></a>
-        <h1 class="logo me-auto"><a href="{{ route('index') }}">PERPUSTAKAAN BPMP KALTIM</a></h1>
+        <a href="{{ route('index') }}" class="logo"><img src="{{ asset('assets_user/img/logo1.jpeg') }}" alt="" class="img-fluid"></a>
+        <h4 class="logo me-auto"><a href="{{ route('index') }}">PERPUSTAKAAN <br> BPMP KALTIM</a></h4>
         <!-- Uncomment below if you prefer to use an image logo -->
 
       <nav id="navbar" class="navbar order-last order-lg-0">
           <ul>
               <li><a class="{{ Request::routeIs('index') ? 'active' : '' }}" href="{{ route('index') }}">Home</a></li>
               <li><a href="{{ route('buku.index') }}" class="{{ Request::routeIs('buku.index') ? 'active' : '' }}">Buku</a></li>
-                <li><a href="courses.html" class="{{ Request::routeIs('index') ? 'active' : '' }}">Courses</a></li>
-                <li><a href="{{ route('wishlist.index') }}"
-                        class="{{ Request::routeIs('wishlist.*') ? 'active' : '' }}">Wishlist</a></li>
-                <li><a href="events.html" class="{{ Request::routeIs('index') ? 'active' : '' }}">Events</a></li>
-                <li><a href="pricing.html" class="{{ Request::routeIs('index') ? 'active' : '' }}">Pricing</a></li>
+                <li><a href="courses.html" class="{{ Request::routeIs('index') ? 'active' : '' }}">Jurnal</a></li>
+                <li><a href="{{ route('wishlist.index') }}" class="{{ Request::routeIs('wishlist.*') ? 'active' : '' }}">Karya </a></li>
 
           {{-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
@@ -32,14 +29,14 @@
               <li><a href="#">Drop Down 4</a></li>
             </ul>
           </li> --}}
-          <li><a href="{{ route('contact.index') }}">Contact</a></li>
+          <li><a href="{{ route('contact.index') }}">Kontak</a></li>
           <li><a href="{{ route('login') }}">Login</a></li>
 
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
 
-      <a href="courses.html" class="get-started-btn">Get Started</a>
+      <a href="{{ route('buku.index') }}" class="get-started-btn">Get Started</a>
 
     </div>
   </header><!-- End Header -->
